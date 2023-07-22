e5a377349802dbdad1bad99446839566cdd5212f
HeyBroooo <mohitkumar4049a@gmail.com>
2023-07-22 09:42:34 +0530

    hey

--- pages/checkout.js
+++ pages/checkout.js
@@ -5,14 +5,65 @@ import {
   AiFillMinusCircle,
 } from "react-icons/ai";
 import { useEffect } from "react";
-
+import Head from "next/head";
+import Script from "next/script";
 
    
 
 
 const Checkout = ({clearCart,cart,addToCart,removeFromCart,subTotal}) => {
+  const initiatePayment =async() => {
+    
+    let oid = Math.floor(Math.random() * Date.now());
+    //get transaction
+
+    const data = { cart, subTotal, oid, email: "email" };
+    let a = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/pretransaction`,{
+          method: 'POST', // or 'PUT'
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+      let txnRes = await a.json()
+      console.log(txnRes)
+      let txnToken = txnRes.txnToken
+    
+
+
+      var config = {
+      "root": "",
+      "flow": "DEFAULT",
+      "data": {
+      "orderId": oid, /* update order id */
+      "token": txnToken, /* update token value */
+      "tokenType": "TXN_TOKEN",
+      "amount": subTotal/* update amount */
+      },
+      "handler": {
+      "notifyMerchant": function(eventName,data){
+      console.log("notifyMerchant handler function called");
+      console.log("eventName => ",eventName);
+      console.log("data => ",data);
+      }
+      }
+      };
+    
+      window.Paytm.CheckoutJS.init(config).then(function onSuccess() {
+      // after successfully updating configuration, invoke JS Checkout
+      window.Paytm.CheckoutJS.invoke();
+      }).catch(function onError(error){
+      console.log("error => ",error);
+      });
+    
+  }
   return (
     <div className="container px-2 sm:m-auto">
+      <Head><meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0"/></Head>
+
+      <Script type="application/javascript" src={`${process.env.NEXT_PUBLIC_PAYTM_HOST}/merchantpgpui/checkoutjs/merchants/${process.env.NEXT_PUBLIC_PAYTM_MID}.js`}
+        crossorigin="anonymous" />
+
       <h1 className="font-bold text-3xl my-8 text-center">Checkout</h1>
       <h2 className="font-semibold text-xl">1. Delivery Details</h2>
       <div className="mx-auto flex my-4">
@@ -166,9 +217,9 @@ const Checkout = ({clearCart,cart,addToCart,removeFromCart,subTotal}) => {
         </ol>
       </div>
       <div className="mx-4">
-      <button className="flex mr-2 text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-500 rounded text-sm">
+      <Link href="https://paytm.me/540-aFf"> <button onClick={initiatePayment} className="flex mr-2 text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-500 rounded text-sm">
     Pay ₹ {subTotal}
-  </button>
+  </button></Link>
   </div>
     </div>
     
