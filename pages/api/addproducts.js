85a27ad4241ea1bfc34b8e1432ac224777cce2b6
HeyBroooo <mohitkumar4049a@gmail.com>
2023-08-09 18:55:01 +0530

    making-dashboard

--- pages/api/addproducts.js
+++ pages/api/addproducts.js
@@ -1,32 +1,32 @@
-import Product from "../../models/Product";
-import connectDb from "../../middleware/mongoose";
+// import Product from "../../models/Product";
+// import connectDb from "../../middleware/mongoose";
 
-const handler = async (req, res) => {
-  if (req.method == 'POST') {
-    try {
-      const products = req.body.map((productData) => {
-        return new Product({
-          title: productData.title,
-          slug: productData.slug,
-          desc: productData.desc,
-          img: productData.img,
-          category: productData.category,
-          size: productData.size,
-          color: productData.color,
-          price: productData.price,
-          availableQty: productData.availableQty,
-        });
-      });
+// const handler = async (req, res) => {
+//   if (req.method == 'POST') {
+//     try {
+//       const products = req.body.map((productData) => {
+//         return new Product({
+//           title: productData.title,
+//           slug: productData.slug,
+//           desc: productData.desc,
+//           img: productData.img,
+//           category: productData.category,
+//           size: productData.size,
+//           color: productData.color,
+//           price: productData.price,
+//           availableQty: productData.availableQty,
+//         });
+//       });
 
-      await Product.insertMany(products);
+//       await Product.insertMany(products);
 
-      res.status(200).json({ success: "Products saved successfully" });
-    } catch (error) {
-      res.status(500).json({ error: "Failed to save products" });
-    }
-  } else {
-    res.status(400).json({ error: "This method is not allowed" });
-  }
-};
+//       res.status(200).json({ success: "Products saved successfully" });
+//     } catch (error) {
+//       res.status(500).json({ error: "Failed to save products" });
+//     }
+//   } else {
+//     res.status(400).json({ error: "This method is not allowed" });
+//   }
+// };
 
-export default connectDb(handler);
+// export default connectDb(handler);
--- pages/api/getproducts.js
+++ pages/api/getproducts.js
@@ -1,12 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import Product from "../../models/Product"
-import connectDb from "../../middleware/mongoose"
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import Product from "../../models/Product"
+// import connectDb from "../../middleware/mongoose"
 
-const handler =  async  (req, res)=> {
-    let products = await Product.find()
+// const handler =  async  (req, res)=> {
+//     let products = await Product.find()
 
-    res.status(200).json({ name: 'mohit kumar' })
-  }
+//     res.status(200).json({ name: 'mohit kumar' })
+//   }
   
 
-export default connectDb(handler);
+// export default connectDb(handler);
