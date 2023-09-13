37b0d716d247c289ba8747e9f9561ff5c5db90af
HeyBroooo <mohitkumar4049a@gmail.com>
2023-09-13 15:56:12 +0530

    changes in form

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import Head from "next/head";
+import { Form } from "react-router-dom";
 
+function formsubmit(e) {
+  e.preventDefault();
+  console.log("form submitted");
+
+  Email.js.sendForm('service_xovqu5c', )
+}
 
 export default function Home() {
 
@@ -500,7 +507,7 @@ export default function Home() {
           </div>
         </section>
       </div>
-
+            <form onSubmit={formsubmit}>
       <div>
         <section class="text-gray-600 body-font relative">
           <div class="container px-5 py-24 mx-auto">
@@ -513,6 +520,7 @@ export default function Home() {
                 gentrify.
               </p>
             </div>
+            
             <div class="lg:w-1/2 md:w-2/3 mx-auto">
               <div class="flex flex-wrap -m-2">
                 <div class="p-2 w-1/2">
@@ -536,7 +544,7 @@ export default function Home() {
                     <input
                       type="email"
                       id="email"
-                      name="email"
+                      name="user_email"
                       class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                     />
                   </div>
@@ -555,17 +563,21 @@ export default function Home() {
                       class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                     ></textarea>
                   </div>
+                  
                 </div>
+                
                 <div class="p-2 w-full">
                   <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                     Button
                   </button>
                 </div>
+                
                 <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
                   <a class="text-indigo-500">example@email.com</a>
                   <p class="leading-normal my-5">
                     49 Smith St. Saint Cloud, MN 56301
                   </p>
+                  
                   <span class="inline-flex">
                     <a class="text-gray-500">
                       <svg
@@ -631,6 +643,7 @@ export default function Home() {
           </div>
         </section>
       </div>
+      </form>
     </div>
   );
 }
