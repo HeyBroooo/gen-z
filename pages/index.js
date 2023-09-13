5f73bb8da0e8247de4bd1248d92c6f138576f85e
HeyBroooo <mohitkumar4049a@gmail.com>
2023-09-13 16:16:26 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -191,9 +191,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          {/* <button class="flex mx-auto mt-2 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            Button
-          </button> */}
+       
         </div>
       </section>
       <div>
