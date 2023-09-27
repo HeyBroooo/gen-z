b4d9943f2157a7723bed976bb574ecfb3deb0a71
HeyBroooo <mohitkumar4049a@gmail.com>
2023-09-27 15:24:23 +0530

    changess

--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ export default function Home() {
               </div>
             </div>
             <div class="xl:w-1/3 md:w-1/2 p-4">
-              <div class="border border-gray-200 p-6 rounded-lg">
+              <div class="border border-gray-200 p-6 rounded-lg hover:scale-110 hover:-translate-y-1 transition ease-in-out delay-150">
                 <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                   <svg
                     fill="none"
@@ -101,7 +101,7 @@ export default function Home() {
               </div>
             </div>
             <div class="xl:w-1/3 md:w-1/2 p-4">
-              <div class="border border-gray-200 p-6 rounded-lg">
+              <div class="border border-gray-200 p-6 rounded-lg hover:scale-110 hover:-translate-y-1 transition ease-in-out delay-150">
                 <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                   <svg
                     fill="none"
@@ -126,7 +126,7 @@ export default function Home() {
               </div>
             </div>
             <div class="xl:w-1/3 md:w-1/2 p-4">
-              <div class="border border-gray-200 p-6 rounded-lg">
+              <div class="border border-gray-200 p-6 rounded-lg hover:scale-110 hover:-translate-y-1 transition ease-in-out delay-150">
                 <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                   <svg
                     fill="none"
@@ -150,7 +150,7 @@ export default function Home() {
               </div>
             </div>
             <div class="xl:w-1/3 md:w-1/2 p-4">
-              <div class="border border-gray-200 p-6 rounded-lg">
+              <div class="border border-gray-200 p-6 rounded-lg hover:scale-110 hover:-translate-y-1 transition ease-in-out delay-150">
                 <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                   <svg
                     fill="none"
@@ -174,7 +174,7 @@ export default function Home() {
               </div>
             </div>
             <div class="xl:w-1/3 md:w-1/2 p-4">
-              <div class="border border-gray-200 p-6 rounded-lg">
+              <div class="border border-gray-200 p-6 rounded-lg hover:scale-110 hover:-translate-y-1 transition ease-in-out delay-150">
                 <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                   <svg
                     fill="none"
@@ -339,7 +339,7 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg hover:scale-110 hover:-translate-y-1 transition ease-in-out delay-150">
               Button
             </button>
           </div>
