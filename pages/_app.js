0d52d4f9e8cf0b8c4de8c6011d050a78d56613cf
HeyBroooo <mohitkumar4049a@gmail.com>
2023-11-19 15:36:05 +0530

    clerk test1

--- pages/_app.js
+++ pages/_app.js
@@ -82,7 +82,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-       <ClerkProvider>
+       <ClerkProvider publishableKey="pk_test_ZmluZS10aWNrLTk3LmNsZXJrLmFjY291bnRzLmRldiQ">
       <LoadingBar
         color="#800080"
         progress={progress}
